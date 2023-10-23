c3ff406ebdb1bdc6fb8d14bf6df06e2b96655f82
Alece-98 <122903839+Alece-98@users.noreply.github.com>
2023-10-23 16:21:59 +0200

    Add files via upload

--- gastenboek2.php
+++ gastenboek2.php
@@ -11,18 +11,17 @@ Naam :<input type="text" name="name"><br>
 </html> 
 
 <?php 
-    include 'guestbook.txt'; 
-    $file = 'guestbook.txt';
+    $file = 'guestbook.json';
     $text = file_get_contents($file, true);
-
+    $fileInput = json_decode($text, true);
     $nameInput = htmlspecialchars($_POST['name']); 
     $messageInput = $_POST['textarea'];
+//    If fill empty put 2 brackets in then circle back
+    $nameAndMessage = array ("naam" => $nameInput, "message" => $messageInput);
+    array_push($fileInput,$nameAndMessage);
 
 
-    $nameAndMessage = array("naam" => $nameInput, "message" => $messageInput);
-
-    $encode = json_encode($nameAndMessage);
-
+    $encode = json_encode($fileInput);
     file_put_contents($file, $encode);
     echo $encode;
 
